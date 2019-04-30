2faa8d83629c1778a258577ce852f71904d5d422
CarlosRomeroo <48665806+CarlosRomeroo@users.noreply.github.com>
2019-04-29 21:30:28 -0600

    4

--- js/acciones.js
+++ js/acciones.js
@@ -3,11 +3,11 @@
 $(document).ready(function (e){
 document.addEventListener ("deviceready",function(){
 		
-$('#Beep').tap(function(){
+$('#beep').tap(function(){
 navigator.notification.beep(3);
 });//tap beep
 		
-$('#Vibrar').tap(function(){
+$('#vibrar').tap(function(){
 navigator.notification.vibrate(1000);
 });//tap vibrar
 		
